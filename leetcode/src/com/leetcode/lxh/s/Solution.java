33c26a3c1d151154108812a4326cb9c0fddea7b1
lxh <codinglixh@gmail.com>
2022-04-01 23:13:00 +0800

    1.两数之和

--- leetcode/src/com/leetcode/lxh/s/Solution.java
+++ leetcode/src/com/leetcode/lxh/s/Solution.java
@@ -26,4 +26,15 @@ public class Solution {
         }
         return nums;
     }
+
+    public int[] twoSum2(int[] nums,int target) {
+        for(int i=0;i<nums.length; i++){
+            for(int j= i+1;j<nums.length-1;j++){
+                if(nums[i]+nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{0};
+    }
 }
